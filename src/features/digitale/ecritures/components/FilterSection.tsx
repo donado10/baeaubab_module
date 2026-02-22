2d6ce950cab873918d3757a5d26c01b53fa8eb79
Ado <adamscoulibaly10@hotmail.com>
2026-02-22 16:07:25 +0000

    update

--- src/features/digitale/ecritures/components/FilterSection.tsx
+++ src/features/digitale/ecritures/components/FilterSection.tsx
@@ -11,10 +11,12 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 import { ReactNode } from "react"
+import { useEcritureEnteteLigneStore } from "../store/store"
 
 
 
 const PopoverFilter = () => {
+    const store = useEcritureEnteteLigneStore()
     return (
         <Collapsible>
             <CollapsibleTrigger ><h1 className="text-blue-600"> Invalide</h1></CollapsibleTrigger>
@@ -23,62 +25,135 @@ const PopoverFilter = () => {
                     <div className="flex flex-col gap-4">
                         <div className="flex items-center gap-3">
                             <Checkbox id="er_equilibre" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "Balanced"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "Balanced")] })
+
                             }} />
                             <Label htmlFor="er_equilibre">Equilibre</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="er_compliance" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "Compliance"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "Compliance")] })
                             }} />
                             <Label htmlFor="er_compliance">Conforme</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_date_journal" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "JM_Date"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "JM_Date")] })
                             }} />
                             <Label htmlFor="err_date_journal">Date Journal</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_ec_jour" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "EC_Jour"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "EC_Jour")] })
                             }} />
                             <Label htmlFor="err_ec_jour">Jour</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_ec_date" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "EC_Date"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "EC_Date")] })
                             }} />
                             <Label htmlFor="err_ec_date">Date écriture</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_ec_piece" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "EC_Piece"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "EC_Piece")] })
                             }} />
                             <Label htmlFor="err_ec_piece">Piece</Label>
                         </div></div>
                     <div className="flex flex-col gap-4">
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_ec_refpiece" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "EC_Refpiece"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "EC_Refpiece")] })
                             }} />
                             <Label htmlFor="err_ec_refpiece">Référence écriture</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_cg_num" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "CG_Num"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "CG_Num")] })
                             }} />
                             <Label htmlFor="err_cg_num">Compte Général</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_ct_num" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "CT_Num"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "CT_Num")] })
                             }} />
                             <Label htmlFor="err_ct_num">Compte tiers</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_ec_intitule" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "EC_Intitule"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "EC_Intitule")] })
                             }} />
                             <Label htmlFor="err_ec_intitule">Intitulé écriture</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_ec_sens" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "EC_Sens"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "EC_Sens")] })
                             }} />
                             <Label htmlFor="err_ec_sens">Sens écriture</Label>
                         </div>
                         <div className="flex items-center gap-3">
                             <Checkbox id="err_ec_montant" onCheckedChange={(e) => {
+                                if (e.valueOf() === true) {
+
+                                    store.setFilter({ ...store.filter, invalide: [...store.filter.invalide, "EC_Montant"] })
+                                    return
+                                }
+                                store.setFilter({ ...store.filter, invalide: [...store.filter.invalide.filter((f) => f !== "EC_Montant")] })
                             }} />
                             <Label htmlFor="err_ec_montant">Montant écriture</Label>
                         </div></div>
--- src/features/digitale/ecritures/components/TableContainer.tsx
+++ src/features/digitale/ecritures/components/TableContainer.tsx
@@ -5,38 +5,125 @@ import { DataTable } from "./Table/table";
 
 import { EStatus, useEcritureEnteteLigneStore } from "../store/store";
 import { DialogTableDetail } from "./DialogTableDetail";
+import { IEcritureEnteteLigne, IEcritureError } from "../interface";
 
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+const filterInvalide = (items: IEcritureEnteteLigne, invalide: string[]) => {
+
+  const filterByBalanced = invalide.includes("Balanced") ? items.filter((ec) => {
+    return ec.error[0].Balanced == "0"
+
+  }) : [...items]
+
+  const filterByCompliance = invalide.includes("Compliance") ? filterByBalanced.filter((ec) => {
+    return ec.error[0].Compliance == "0"
+
+  }) : [...filterByBalanced]
+
+  const filterByJMDate = invalide.includes("JM_Date") ? filterByCompliance.filter((ec) => {
+    return ec.error[0].JM_Date == "0"
+
+  }) : [...filterByCompliance]
+
+  const filterByECJour = invalide.includes("EC_Jour") ? filterByJMDate.filter((ec) => {
+    return ec.error[0].EC_Jour == "0"
+
+  }) : [...filterByJMDate]
+
+  const filterByECDate = invalide.includes("EC_Date") ? filterByECJour.filter((ec) => {
+    return ec.error[0].EC_Date == "0"
+
+  }) : [...filterByECJour]
+
+  const filterByECPiece = invalide.includes("EC_Piece") ? filterByECDate.filter((ec) => {
+    return ec.error[0].EC_Piece == "0"
+
+  }) : [...filterByECDate]
+
+  const filterByECRefPiece = invalide.includes("EC_Refpiece") ? filterByECPiece.filter((ec) => {
+    return ec.error[0].EC_RefPiece == "0"
+
+  }) : [...filterByECPiece]
+
+  const filterByCGNum = invalide.includes("CG_Num") ? filterByECRefPiece.filter((ec) => {
+    return ec.error[0].CG_Num == "0"
+
+  }) : [...filterByECRefPiece]
+
+  const filterByCTNum = invalide.includes("CT_Num") ? filterByCGNum.filter((ec) => {
+    return ec.error[0].CT_Num == "0"
+
+  }) : [...filterByCGNum]
+
+  const filterByECIntitule = invalide.includes("EC_Intitule") ? filterByCTNum.filter((ec) => {
+    return ec.error[0].EC_Intitule == "0"
+
+  }) : [...filterByCTNum]
+
+  const filterByECSens = invalide.includes("EC_Sens") ? filterByECIntitule.filter((ec) => {
+    return ec.error[0].EC_Sens == "0"
+
+  }) : [...filterByECIntitule]
+
+  const filterByECMontant = invalide.includes("EC_Montant") ? filterByECSens.filter((ec) => {
+    return ec.error[0].EC_Montant == "0"
+
+  }) : [...filterByECSens]
+
+
+
+  return filterByECMontant
+}
 
 const TableEcritureDigitalContainer = () => {
 
-  const EcritureStore = useEcritureEnteteLigneStore()
+  const store = useEcritureEnteteLigneStore()
   const EcMapByStatus = new Map<number, EStatus>([[0, EStatus.ATTENTE], [1, EStatus.INVALIDE], [2, EStatus.VALIDE], [3, EStatus.INTEGRE]])
-  const [ecritures, setEcritures] = useState(EcritureStore.items)
+  const [ecritures, setEcritures] = useState(store.items)
 
   useEffect(() => {
-    const filterByStatus = EcritureStore.filter?.status !== EStatus.ALL ? EcritureStore.items.filter((ec) => {
-      if (EcMapByStatus.get(ec.entete.Status) === EcritureStore.filter?.status) {
-        return ec
-      }
-    }) : [...EcritureStore.items]
 
-    const filterBySearch = EcritureStore.filter.search?.value ? filterByStatus.filter((value) => {
-      if (EcritureStore.filter.search.type === 'facture') {
-
-        return value.entete.EC_RefPiece.toLowerCase().includes(EcritureStore.filter.search.value.toLowerCase())
-      }
-      if (EcritureStore.filter.search.type === 'tiers') {
-
-        return value.entete.CT_Num && value.entete.CT_Num.toLowerCase().includes(EcritureStore.filter.search.value.toLowerCase())
-      }
-    }) : [...filterByStatus]
-
-
-    setEcritures(filterBySearch)
+    const filterEc = async () => {
 
 
 
-  }, [JSON.stringify(EcritureStore.filter), JSON.stringify(EcritureStore.items)])
+
+      const filterByStatus = store.filter?.status !== EStatus.ALL ? store.items.filter((ec) => {
+        if (EcMapByStatus.get(ec.entete.Status) === store.filter?.status) {
+          return ec
+        }
+      }) : [...store.items]
+
+      /*       await sleep(3000)
+       */
+      const filterBySearch = store.filter.search?.value ? filterByStatus.filter((value) => {
+        if (store.filter.search.type === 'facture') {
+
+          return value.entete.EC_RefPiece.toLowerCase().includes(store.filter.search.value.toLowerCase())
+        }
+        if (store.filter.search.type === 'tiers') {
+
+          return value.entete.CT_Num && value.entete.CT_Num.toLowerCase().includes(store.filter.search.value.toLowerCase())
+        }
+      }) : [...filterByStatus]
+
+      const filterByInvalide = filterInvalide(filterBySearch, store.filter.invalide)
+
+
+
+      setEcritures(filterByInvalide)
+
+    }
+
+    filterEc()
+
+
+  }, [JSON.stringify(store.filter), JSON.stringify(store.items)])
 
 
   return (
@@ -47,7 +134,7 @@ const TableEcritureDigitalContainer = () => {
           ecritures.map((val) => val.entete)
         }
       />
-      {<DialogTableDetail refpiece={EcritureStore.dialog.viewTable[1]} open={EcritureStore.dialog.viewTable[0]} setOpen={(value) => EcritureStore.setDialogState({ ...EcritureStore.dialog, viewTable: [value, ''] })} />}
+      {<DialogTableDetail refpiece={store.dialog.viewTable[1]} open={store.dialog.viewTable[0]} setOpen={(value) => store.setDialogState({ ...store.dialog, viewTable: [value, ''] })} />}
 
     </div>
   );
